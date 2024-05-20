6b225e03e5178911e372489ace7e6cbf532754aa
Garrett <Garrett@YUP>
2024-05-20 15:50:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput = Console.ReadLine();
+
+            int spaces = SpaceCounter(stringInput);
+
+            Console.WriteLine(spaces);
+        }
+
+        public static int SpaceCounter(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == ' ') count++;
+            }
+            return count;
+        }
+
+    }
+}
